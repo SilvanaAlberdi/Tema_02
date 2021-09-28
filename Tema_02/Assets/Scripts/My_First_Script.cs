af982fe5049d1b81d5881e09a1fc32d049da8d61
SilvanaAlberdi <silvanaalberdi@ni.eus>
2021-09-28 10:56:44 +0200

    Tema_02

---
+++ Tema_02/Assets/Scripts/My_First_Script.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class My_First_Script : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Oh, así que así es cómo se ven los comentarios en el código... Qué guay...
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
